ee3d1cbcc8b41b8835dc4b877a863a847343b955
chetanukani <ukanichetan007@gmail.com>
2024-03-16 14:32:41 +0530

    Add: side drawer

---
+++ frontend/src/routes.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Home from './components/home';
+import Header from './components/navigation/header';
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path='/' element={<Home></Home>} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default Router;
